07cda2f3496422b43e1f887f8bb3d3b39bb6691e
DAXTER1829 <diegoazarate110800@gmail.com>
2025-09-29 17:57:09 -0600

    ajustes

--- play.js
+++ play.js
@@ -4,8 +4,6 @@ let ctrlIcon = document.getElementById("ctrlIcon");
 let currentTimeDisplay = document.getElementById("current-time");
 let durationTimeDisplay = document.getElementById("duration-time");
 
-let updateInterval;
-
 function formatTime(seconds) {
     const minutes = Math.floor(seconds / 60);
     const remainingSeconds = Math.floor(seconds % 60).toString().padStart(2, '0');
@@ -18,44 +16,38 @@ song.onloadedmetadata = function() {
     durationTimeDisplay.textContent = formatTime(song.duration);
 }
 
+song.ontimeupdate = function() {
+    if (!progress.dragging) {
+        progress.value = song.currentTime;
+        currentTimeDisplay.textContent = formatTime(song.currentTime);
+    }
+}
+
+song.onended = function() {
+    ctrlIcon.classList.remove("fa-pause");
+    ctrlIcon.classList.add("fa-play");
+    progress.value = 0;
+    currentTimeDisplay.textContent = '0:00';
+}
+
 function playPause(){
     if(ctrlIcon.classList.contains("fa-pause")){
         song.pause();
         ctrlIcon.classList.remove("fa-pause");
         ctrlIcon.classList.add("fa-play");
-        clearInterval(updateInterval); 
     } else {
         song.play();
         ctrlIcon.classList.add("fa-pause");
         ctrlIcon.classList.remove("fa-play");
-        
-        updateInterval = setInterval(() => {
-            progress.value = song.currentTime;
-            currentTimeDisplay.textContent = formatTime(song.currentTime);
-            
-            if (song.ended) {
-                clearInterval(updateInterval);
-                ctrlIcon.classList.remove("fa-pause");
-                ctrlIcon.classList.add("fa-play");
-                progress.value = 0;
-                currentTimeDisplay.textContent = '0:00';
-            }
-        }, 500);
     }
 }
 
+progress.oninput = function(){
+    progress.dragging = true;
+    currentTimeDisplay.textContent = formatTime(progress.value);
+}
+
 progress.onchange = function(){
-    song.currentTime = progress.value; 
-    
-    if (song.paused) {
-        song.play();
-        ctrlIcon.classList.add("fa-pause");
-        ctrlIcon.classList.remove("fa-play");
-        
-        if (!updateInterval) {
-            playPause(); 
-        }
-    }
-    
-    currentTimeDisplay.textContent = formatTime(song.currentTime); 
+    song.currentTime = progress.value;
+    progress.dragging = false;
 }
